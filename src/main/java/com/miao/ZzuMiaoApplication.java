6e8537dd7b3a6fa40db7ded27372bd0e161c78c1
dhx_ <dhx2648466390@163.com>
2022-10-22 11:59:07 +0800

    Initial commit

---
+++ src/main/java/com/miao/ZzuMiaoApplication.java
@@ -0,0 +1,13 @@
+package com.miao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzuMiaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZzuMiaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/miao/ZzuMiaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzuMiaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
